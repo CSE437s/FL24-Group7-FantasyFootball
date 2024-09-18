7a7ce26835a3ba499d81dc26ba6e7216d89b06e8
RandyDi <randydiaz9050@gmail.com>
2024-09-17 19:28:01 -0500

    Added Audio
    - Added Event Listener to listen for User interaction to start Window.
    -

--- fantasyfootball/app/page.tsx
+++ fantasyfootball/app/page.tsx
@@ -1,35 +1,63 @@
-"use client"; 
+"use client";
 import { NextPage } from "next";
 import styles from '../styles/homePageLayout.module.css';
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Button } from "@mui/material";
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
-
+import ReactAudioPlayer from 'react-audio-player';
 
 const HomePage: NextPage = () => {
+  const [isMute, setIsMute] = useState(false);
+  const audioRef = useRef<ReactAudioPlayer>(null);
 
-  const [isMute, setIsMute] = useState<boolean>(false);
+  const toggleMute = () => {
+    setIsMute(!isMute);
+  };
 
+  const handleUserInteraction = () => {
+    if (audioRef.current && audioRef.current.audioEl.current) {
+      audioRef.current.audioEl.current.play();
+    }
+  };
+
+  useEffect(() => {
+    // Trigger autoplay by simulating user interaction
+    const handleInteraction = () => {
+      handleUserInteraction();
+      window.removeEventListener('click', handleInteraction);
+      window.removeEventListener('keypress', handleInteraction);
+    };
+
+    // Listen for user interaction to enable sound
+    window.addEventListener('click', handleInteraction);
+    window.addEventListener('keypress', handleInteraction);
+
+    return () => {
+      window.removeEventListener('click', handleInteraction);
+      window.removeEventListener('keypress', handleInteraction);
+    };
+  }, []);
   
 
-  const muteSound = () => {
-    setIsMute(false);
-  }
   return (
     <>
-    <main>
-    <div className={styles.App}>
-      <h1 className={styles.title}>Hello World</h1>
-      {isMute ? (
-        <Button onClick={muteSound}><VolumeMuteIcon/></Button>
-      ): (
-        <Button onClick={muteSound}><VolumeUpIcon/></Button>
-      )}
-    </div>
-    </main>
+      <main>
+        <div className={styles.App}>
+          <h1 className={styles.title}>Hello World</h1>
+          <ReactAudioPlayer
+            src="/HomePage/FoxFootballSong.mp3"
+            ref={audioRef}
+            autoPlay
+            muted={isMute}
+          />
+          <Button onClick={toggleMute}>
+            {isMute ? <VolumeMuteIcon /> : <VolumeUpIcon />}
+          </Button>
+        </div>
+      </main>
     </>
   );
-}
+};
 
 export default HomePage;
